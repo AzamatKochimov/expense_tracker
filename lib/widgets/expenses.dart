db21aa7f6a70dd2d4a544b3d34b4c3fbb47e7ff9
AzamatKochimov <kochimovazamat180@gmail.com>
2023-11-27 05:31:28 +0500

    Updated and added new chart bar

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -34,19 +35,20 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
   }
 
-  void _addExpense(Expense expense){
+  void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
     });
   }
 
-  void _removeExpense(Expense expense){
+  void _removeExpense(Expense expense) {
     final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
@@ -68,35 +70,49 @@ class _ExpensesState extends State<Expenses> {
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    Widget mainContent = const Center(child: Text('No expense found, start adding some!'),);
+    final width = MediaQuery.of(context).size.width;
 
-    if(_registeredExpenses.isNotEmpty){
-      mainContent = ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,);
+    Widget mainContent = const Center(
+      child: Text('No expense found, start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
     }
-
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Expense Tracker"),
         actions: [
-          IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          const Text('The Chart...'),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }
